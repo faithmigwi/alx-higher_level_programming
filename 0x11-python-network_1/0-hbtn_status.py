2cd3ce915a0ff57fee00ef5d7b8c824285bd3738
faithmigwi <faithmigwi35@gmail.com>
2023-12-01 10:50:42 -0800

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status."""
-
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
 from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as page:
-        content = page.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    the_page = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
